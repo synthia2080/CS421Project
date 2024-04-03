08c371631ad09fb88f86edc5527b44cbaa7cfc96
Synthia S <cynthiasynthia2077@gmail.com>
2024-04-03 17:29:42 -0500

    started num_sentences()

--- sample_code_1.py
+++ sample_code_1.py
@@ -1,2 +1,24 @@
 import numpy as np
 import nltk
+from nltk import tokenize, word_tokenize
+from spellchecker import SpellChecker
+
+def num_sentences(txt):
+    #Get base number of sentences through sentence tokenizer
+    tokenized_sentences = tokenize.sent_tokenize(txt)
+    base_num = len(tokenized_sentences)
+
+    #Check for any missed "implied sentences"
+    tokenized_words = word_tokenize(txt)
+    POS_tags = nltk.pos_tag(tokenized_words)
+    print(POS_tags)
+    
+
+
+
+def spelling_mistakes(txt):
+    pass
+
+
+test = "Hello, I am Synthia"
+print(num_sentences(test))
