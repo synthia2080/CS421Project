709cc44755643c4e3da9a76eac56bc92f6054e0a
Synthia S <cynthiasynthia2077@gmail.com>
2024-04-09 22:01:28 -0500

    cleaned up code a bit

--- sample_code_1.py
+++ sample_code_1.py
@@ -14,18 +14,15 @@ def num_sentences(txt):
     txt: essay as a string
     returns: score as an int
     """
-
     #Get base number of sentences through sentence tokenizer
     tokenized_sentences = tokenize.sent_tokenize(txt)
-    tokenized_sentences_len = len(tokenized_sentences)
 
     comma_list = [',', ';']
-    not_EOS_pos_tags = ["DT","IN", "CC","DT", "PRP$", "WP$", "IN", "VB", "VBP", "VBZ", "VBD"]
-
-    # Checking for missed sentences
+    not_EOS_pos_tags = ["DT","IN", "CC","DT", "PRP$", "WP$", "IN", "VB", "VBP", "VBZ", "VBD"] #Tags that shouldn't be at end of sentence
     new_sentences = [] #holds all new sentences minus the last one
 
-    for i, s in enumerate(tokenized_sentences):
+    # Checking for missed sentences
+    for s in tokenized_sentences:
         #get POS tags for analyzing
         tokenized_words = word_tokenize(s)
         POS_tags = nltk.pos_tag(tokenized_words)
@@ -34,33 +31,31 @@ def num_sentences(txt):
         holder_sentence = ""
 
         #Go through each word & its POS tag
-        for i2, w in enumerate(POS_tags):
+        for i2, (word, tag) in enumerate(POS_tags):
             if i2 == 0:
-                holder_sentence += f"{w[0]} "
+                holder_sentence += f"{word} "
                 continue
-
-            word = w[0]
-            tag = w[1]
             
             word_before = POS_tags[i2-1][0]
             tag_before = POS_tags[i2-1][1]
+
             #Assume capitalized, non-proper nouns is a marker for new sentence
             if word[0].isupper() and tag != 'NNP' and word_before not in comma_list and tag_before not in not_EOS_pos_tags:
                 new_sentences.append(holder_sentence)
-                # tokenized_sentences_copy.remove(s)
                 holder_sentence = ""
 
             holder_sentence += f"{word} "
 
         new_sentences.append(holder_sentence)
 
+
     notfiniteVerbTags = ['VB', 'VBG', 'VBN']
-    sub_coord_tags = ['CC', 'IN', 'WP', 'WDT', 'WP$', 'RB']
+    sub_coord_tags = ['CC', 'IN', 'WP', 'WDT', 'WP$', 'RB'] #Tags usually indicating subordinate/coordinate clauses
     num_missed_sentences = 0
     num_sentences_offset = 0
 
     #Check for more missed sentences based on finite verbs
-    for i, s in enumerate(new_sentences):
+    for s in new_sentences:
         tokenized_words = word_tokenize(s)
         POS_tags = nltk.pos_tag(tokenized_words)
 
@@ -73,28 +68,18 @@ def num_sentences(txt):
         sum_sub_coord_tags = sum(1 for t in POS_tags if t[1] in sub_coord_tags)
         sum_and_words = sum(1 for t in POS_tags if t[0].lower() == "and") #count number of ands
 
-        #If the there are more finite verbs than counted sub/coord clauses, it should indicate a missed sentence
+        #More finite verbs than counted sub/coord clauses, should indicate a missed sentence
         if sum_sub_coord_tags < num_finite_verbs:
-            #add 1 as offset for corrent missed count if word includes an "and"
+            #Prevent extra sentence count for sentences with "and"
             if sum_and_words != 0:
                 sum_and_words += 1
+
             missed = (num_finite_verbs-sum_and_words)
             num_missed_sentences += missed
             if missed > 0:
                 num_sentences_offset += 1 #offset used later to not count this current sentence
 
-
-        # print()
-        # print(f"num_finite: {num_finite_verbs} || SCV: {sum_sub_coord_tags} || num_missed {(num_finite_verbs-sum_sub_coord_tags-1)}|| {s}")
-        # print(POS_tags)
-
-
     num_sentences = num_missed_sentences + (len(new_sentences) - num_sentences_offset)
-
-    # print()
-    # print(f"tokenized sentences: {tokenized_sentences}")
-    # print(f"new sentences: {new_sentences}")
-    # print(f"num sentences: {num_sentences} || tokenized_senteces: {tokenized_sentences_len} || new_sentence: {len(new_sentences)} || num_missed: {num_missed_sentences}")
 
     #Return score based on where number of sentences falls in the range
     # if num_sentences < 10:
@@ -109,7 +94,7 @@ def num_sentences(txt):
     #     return 4
     # else:
     #     return 5
-    
+
     if num_sentences <= 0:
         return 0
     elif num_sentences >= 22:
@@ -117,7 +102,6 @@ def num_sentences(txt):
     else:
         return (num_sentences**2 / 22.0**2) * 5
 
-    # return num_sentences
 
 
 def spelling_mistakes(txt):
@@ -125,12 +109,13 @@ def spelling_mistakes(txt):
 
 
 def getAverageSentCount():
-    #
-    #Getting averages of number of sentences for high and low grades
-    #
+    """
+    Gets averages of number of sentences for high and low grades
+    """
     index = pd.read_csv("essays_dataset/index.csv", delimiter=";")
     pathToEssays = "essays_dataset/essays/"
     essays = []
+
     #read in essays 
     for filename in os.listdir(pathToEssays):
         file_path = os.path.join(pathToEssays, filename)
@@ -145,9 +130,7 @@ def getAverageSentCount():
     high_sentence_num = 0
     high_sentence_totalSentenceNum = 0
     for name, essay in essays:
-
         numSentence = num_sentences(essay)
-
 
         df = index[index['filename'] == name]
         score = df['grade'].iloc[0]
